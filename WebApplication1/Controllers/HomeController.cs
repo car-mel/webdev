d4884ad80016016399d984609b31db6dfde63771
Carmel <22104744@usc.edu.ph>
2023-12-07 20:38:04 +0800

    uploaded a project

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            ViewBag.Message = "Your login page.";
+
+            return View();
+        }
+
+        public ActionResult AddUserToDatabase(FormCollection fc)
+        {
+            String name = fc["name"];
+            String email = fc["email"];
+            String diko = fc["password"];
+
+            user use = new user();
+            use.name = name;
+            use.email = email;
+            use.password = diko;
+            use.role_id = 1;
+
+            usersEntities fe = new usersEntities();
+            fe.users.Add(use);
+            fe.SaveChanges();
+
+            //insert the code that will save these information to the DB
+
+            return RedirectToAction("ShowUser");
+        }
+
+
+
+
+        public ActionResult ShowUser()
+        {
+            usersEntities fe = new usersEntities();
+            var userList = (from a in fe.users
+                            select a).ToList();
+
+            ViewData["UserList"] = userList;
+            return View();
+        }
+    }
+}
